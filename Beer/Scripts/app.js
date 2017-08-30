85dfeb4af5427ff5df49830aa2e2390cfd3eb619
Karen Elliott <btdevil@yahoo.co.uk>
2017-08-30 11:10:27 +0100

    search working with hops, malts and yeasts

--- Beer/Scripts/app.js
+++ Beer/Scripts/app.js
@@ -2,105 +2,210 @@
 
 BEER.utils = (function () {
     var process = {
-    ajaxHelper: function(uri, method, data, self) {
-        self.error(''); // Clear error message
-        return $.ajax({
-            type: method,
-            url: uri,
-            dataType: 'json',
-            contentType: 'application/json',
-            accepts: "application/json",
-            data: data ? JSON.stringify(data) : null
-        }).fail(function (jqXHR, textStatus, errorThrown) {
-            self.error(errorThrown);
-        });
-    }};
+        ajaxHelper: function (uri, method, data, self) {
+            self.error(''); // Clear error message
+            return $.ajax({
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                accepts: "application/json",
+                data: data ? JSON.stringify(data) : null
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                self.error(errorThrown);
+            });
+        }
+    };
     return process;
-})()
+})();
 
-BEER.ViewModel = function () {
+BEER.YeastModel = function (Yeast) {
     var self = this;
-    self.recipes = ko.observableArray();
-    self.malts = ko.observableArray();
-    self.yeast = ko.observableArray();
-    self.hops = ko.observableArray();
-    self.error = ko.observable();
+    self.Yeast = ko.observableArray(Yeast);
+    self.error = ko.observableArray();
 
-    var recipesUri = '/api/recipes/';
-
-    function getAllRecipes() {
-        BEER.utils.ajaxHelper(recipesUri, 'GET', null, self).done(function (data) {
-            self.recipes(data);
-        });
-    }
-
-    // Fetch the initial data.
-    getAllRecipes();
-
-    self.detail = ko.observable();
-
-    self.getRecipeDetail = function (item) {
-        BEER.utils.ajaxHelper(recipesUri + item.id, 'GET', null, self).done(function (data) {
-            self.detail(data);
-        });
-    }
-};
-
-//ko.applyBindings(new BEER.ViewModel(), document.getElementById('recipes'));
-
-BEER.maltViewModel = function () {
-    var that = this;
-    that.malts = ko.observableArray();
-    that.error = ko.observableArray();
-
-    var recipesUri = '/api/maltgenerics/';
-
-    function getAllMalts() {
-        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
-            that.malts(data);
-        });
-    }
-
-    // Fetch the initial data.
-    getAllMalts();
-}
-
-ko.applyBindings(new BEER.maltViewModel(), document.getElementById('malts'));
-
-BEER.hopViewModel = function () {
-    var that = this;
-    that.hops = ko.observableArray();
-    that.error = ko.observableArray();
-
-    var recipesUri = '/api/hops/';
-
-    function getAllHops() {
-        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
-            that.hops(data);
-        });
-    }
-
-    // Fetch the initial data.
-    getAllHops();
-}
-
-ko.applyBindings(new BEER.hopViewModel(), document.getElementById('hops'));
-
-BEER.yeastViewModel = function () {
-    var that = this;
-    that.yeast = ko.observableArray();
-    that.error = ko.observableArray();
-
-    var recipesUri = '/api/yeasts/';
-
+    var serviceUri = '/api/yeasts/';
+    
     function getAllYeast() {
-        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
-            that.yeast(data);
+        BEER.utils.ajaxHelper(serviceUri, 'GET', null, self).done(function (data) {
+            ko.mapping.fromJS(data, {}, self.Yeast);
         });
     }
-
+    
     // Fetch the initial data.
     getAllYeast();
 }
 
-ko.applyBindings(new BEER.yeastViewModel(), document.getElementById('yeast'));
+BEER.MaltModel = function (Malt) {
+    var self = this;
+    self.Malt = ko.observableArray(Malt);
+    self.error = ko.observableArray();
+
+    var serviceUri = '/api/maltgenerics/';
+    
+    function getAllMalts() {
+        BEER.utils.ajaxHelper(serviceUri, 'GET', null, self).done(function (data) {
+            ko.mapping.fromJS(data, {}, self.Malt);
+        });
+    }
+    
+    // Fetch the initial data.
+    getAllMalts();
+}
+
+BEER.HopModel = function (Hop) {
+    var self = this;
+    self.Hop = ko.observableArray(Hop);
+    self.error = ko.observableArray();
+
+    var serviceUri = '/api/hops/';
+    
+    function getAllHops() {
+        BEER.utils.ajaxHelper(serviceUri, 'GET', null, self).done(function (data) {
+            ko.mapping.fromJS(data, {}, self.Hop);
+        });
+    }
+    
+    // Fetch the initial data.
+    getAllHops();
+}
+
+BEER.RecipeModel = function (Recipe) {
+    var self = this;
+    self.Recipe = ko.observableArray(Recipe);
+    self.error = ko.observableArray();
+    self.detail = ko.observable();
+
+    var recipesUri = '/api/recipes/';
+    
+    self.getRecipeDetail = function (item) {
+        BEER.utils.ajaxHelper(recipesUri + item.id(), 'GET', null, self).done(function (data) {
+            self.detail(data);
+        });
+    }
+}
+
+BEER.MasterViewModel = function () {
+    var self = this;
+    self.Yeasts = new BEER.YeastModel();
+    self.Malts = new BEER.MaltModel();
+    self.Hops = new BEER.HopModel();
+    self.Recipes = new BEER.RecipeModel();
+    self.error = ko.observableArray();
+
+    self.searchRecipes = function (formElement) {
+        var searchQuery = "?";
+        console.log("searching");
+
+        $(formElement[0].selectedOptions).each(function () {
+            searchQuery += 'maltid=' + $(this).val() + '&';
+        });
+
+        $(formElement[1].selectedOptions).each(function () {
+            searchQuery += 'hopid=' + $(this).val() + '&';
+        });
+
+        $(formElement[2].selectedOptions).each(function () {
+            searchQuery += 'yeastid=' + $(this).val() + '&';
+        });
+
+        console.log(searchQuery);
+
+        var serviceUri = '/api/recipes' + searchQuery;
+
+        BEER.utils.ajaxHelper(serviceUri, 'GET', null, self).done(function (data) {
+            ko.mapping.fromJS(data, {}, self.Recipes.Recipe);
+        });
+
+        console.log(self.Recipes);
+    }
+}
+
+ko.applyBindings(new BEER.MasterViewModel())
+
+//BEER.ViewModel = function () {
+//    var self = this;
+//    self.recipes = ko.observableArray();
+//    self.malts = ko.observableArray();
+//    self.yeast = ko.observableArray();
+//    self.hops = ko.observableArray();
+//    self.error = ko.observable();
+
+//    var recipesUri = '/api/recipes/';
+
+//    function getAllRecipes() {
+//        BEER.utils.ajaxHelper(recipesUri, 'GET', null, self).done(function (data) {
+//            self.recipes(data);
+//        });
+//    }
+
+//    // Fetch the initial data.
+//    getAllRecipes();
+
+//    self.detail = ko.observable();
+
+//    self.getRecipeDetail = function (item) {
+//        BEER.utils.ajaxHelper(recipesUri + item.id, 'GET', null, self).done(function (data) {
+//            self.detail(data);
+//        });
+//    }
+//};
+
+////ko.applyBindings(new BEER.ViewModel(), document.getElementById('recipes'));
+
+//BEER.maltViewModel = function () {
+//    var that = this;
+//    that.malts = ko.observableArray();
+//    that.error = ko.observableArray();
+
+//    var recipesUri = '/api/maltgenerics/';
+
+//    function getAllMalts() {
+//        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
+//            that.malts(data);
+//        });
+//    }
+
+//    // Fetch the initial data.
+//    getAllMalts();
+//}
+
+//ko.applyBindings(new BEER.maltViewModel(), document.getElementById('malts'));
+
+//BEER.hopViewModel = function () {
+//    var that = this;
+//    that.hops = ko.observableArray();
+//    that.error = ko.observableArray();
+
+//    var recipesUri = '/api/hops/';
+
+//    function getAllHops() {
+//        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
+//            that.hops(data);
+//        });
+//    }
+
+//    // Fetch the initial data.
+//    getAllHops();
+//}
+
+//ko.applyBindings(new BEER.hopViewModel(), document.getElementById('hops'));
+
+//BEER.yeastViewModel = function () {
+//    var that = this;
+//    that.yeast = ko.observableArray();
+//    that.error = ko.observableArray();
+
+//    var recipesUri = '/api/yeasts/';
+
+//    function getAllYeast() {
+//        BEER.utils.ajaxHelper(recipesUri, 'GET', null,that).done(function (data) {
+//            that.yeast(data);
+//        });
+//    }
+
+//    // Fetch the initial data.
+//    getAllYeast();
+//}
+
+//ko.applyBindings(new BEER.yeastViewModel(), document.getElementById('yeast'));
